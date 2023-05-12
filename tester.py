0063deb508ec918c7584a2a9582cbec9385528d8
Alperen <bitirgenalperen@gmail.com>
2023-05-12 18:15:02 +0300

    bigger testcases

--- tester.py
+++ tester.py
@@ -238,8 +238,9 @@ def printTester(testIdx, rep):
 
 
 if __name__ == '__main__':
-    testCaseCount = 10
+    testCaseCount = 15
     testRepeat = 10
+
     for i in range(1,testCaseCount+1):
         # Open the file for reading
         with open(f"inputs/input{i}.txt", 'r') as file:
